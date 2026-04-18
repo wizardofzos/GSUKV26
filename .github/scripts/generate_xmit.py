82da220983e4e072a5405c3608050cb85573c078
Wizard of z/OS <wizard@zdevops.com>
2026-04-18 18:55:05 +0200

    add workflows

---
+++ .github/scripts/generate_xmit.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from pathlib import Path 
+from xmi import create_xmi
+import shutil
+
+
+inputfile = Path('GSUK26V.PDS')
+outputfile = Path('GSUK26V.XMIT')
+
+tmppds = Path(sys.argv[1])
+
+shutil.copy(inputfile, tmppds)
+create_xmi(tmppds, output_file=outputfile, from_user="WIZARD", from_node="GITHUB")
