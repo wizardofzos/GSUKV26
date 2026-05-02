7758a9c4e9265470a0920bc5e96c8d0ed33f8c10
Wizard of z/OS <wizard@zdevops.com>
2026-05-02 21:03:17 +0200

    Updates :)

--- .github/scripts/generate_xmit.py
+++ .github/scripts/generate_xmit.py
@@ -6,4 +6,5 @@ from xmi import create_xmi
 inputfile = Path('GSUK26V.PDS')
 outputfile = Path('GSUK26V.XMIT')
 
-create_xmi(inputfile, output_file=outputfile, from_user="WIZARD", from_node="GITHUB")
+create_xmi(inputfile, output_file=outputfile, from_user="WIZARD",
+           from_node="GITHUB", message="Created with xmi-reader 1.0.3")
